ebf8fea00f2eb2578cbd761a07c7a05184cf21c0
praweshlamsal <praweshlamsalk@gmail.com>
2025-04-11 19:59:39 -0400

    Updates

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -1,23 +1,23 @@
-// firebaseConfig.js
-import { initializeApp } from 'firebase/app';
-import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { getFirestore } from 'firebase/firestore';
+// // firebaseConfig.js
+// import { initializeApp } from 'firebase/app';
+// import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+// import AsyncStorage from '@react-native-async-storage/async-storage';
+// import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDZFaF1dcoFSeVJFVJ3N8tKslVGcJ6zy94",
-    authDomain: "eventorganizerapp-5c072.firebaseapp.com",
-    projectId: "eventorganizerapp-5c072",
-    storageBucket: "eventorganizerapp-5c072.firebasestorage.app",
-    messagingSenderId: "367421196900",
-    appId: "1:367421196900:web:988c132c2c6e50bb1f2d71"
-  };
+// const firebaseConfig = {
+//     apiKey: "AIzaSyDZFaF1dcoFSeVJFVJ3N8tKslVGcJ6zy94",
+//     authDomain: "eventorganizerapp-5c072.firebaseapp.com",
+//     projectId: "eventorganizerapp-5c072",
+//     storageBucket: "eventorganizerapp-5c072.firebasestorage.app",
+//     messagingSenderId: "367421196900",
+//     appId: "1:367421196900:web:988c132c2c6e50bb1f2d71"
+//   };
 
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
 
-export const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(AsyncStorage),
-});
+// export const auth = initializeAuth(app, {
+//   persistence: getReactNativePersistence(AsyncStorage),
+// });
 
 
-export const db = getFirestore(app);
+// export const db = getFirestore(app);
